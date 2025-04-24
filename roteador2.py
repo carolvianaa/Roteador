e2bd3e5944be409d51e88e15577381e78047b337
Caroline Viana <159588473+carolvianaa@users.noreply.github.com>
2025-04-23 21:47:56 -0300

    Update roteador2.py

--- roteador2.py
+++ roteador2.py
@@ -1,69 +1,51 @@
 from datetime import datetime
-from rich import print
 
 class Dispositivo:
     def __init__(self, nome: str, tipo: str):
         self.nome = nome
         self.tipo = tipo
-        self._ip = None  
+        self._ip = None
         self.data_conexao = None
 
-    @property
-    def ip(self):
-        return self._ip
-
-    @ip.setter
-    def ip(self, valor: str):
-        self._ip = valor
-
-    def __str__(self):
-        ip_info = self.ip if self.ip else "Sem IP"
-        return f"{self.tipo} {self.nome} | IP: {ip_info}"
-
-
 class Roteador:
-    def __init__(self, nome: str, ip_base: str, senha: str):
+    def __init__(self, nome: str, ip_base: str, ip: int, senha: str):
         self.nome = nome
-        self._ip_base = ip_base  
-        self._senha = senha
-        self._contador = 1
-        self._conectados: list[Dispositivo] = []
+        self.ip_base = ip_base
+        self.ip = ip
+        self.senha = senha
+        self.contador = ip + 1
+        self.conectados: list[Dispositivo] = []
 
-    @property
-    def ip_base(self):
-        return self._ip_base
 
     def conectar(self, dispositivo: Dispositivo, senha_digitada: str):
-        if senha_digitada != self._senha:
+        if senha_digitada != self.senha:
             print(f"Conexão recusada para {dispositivo.nome}. Senha incorreta.")
             return
 
-        ip_dispositivo = f"{self._ip_base}.{self._contador}"
-        self._contador += 1
-
+        ip_dispositivo = f"{self.ip_base}.{self.contador}"
+        self.contador += 1    
         dispositivo.ip = ip_dispositivo
         dispositivo.data_conexao = datetime.now()
-        self._conectados.append(dispositivo)
+        self.conectados.append(dispositivo)
 
-        print(f"Conectado: {dispositivo} às {dispositivo.data_conexao:%H:%M:%S}")
+        print(f"Conectado: {dispositivo.nome} IP | {dispositivo.ip} às {dispositivo.data_conexao:%H:%M:%S}")
 
     def listar_dispositivos(self):
-        print(f"\nDispositivos conectados ao roteador {self.nome}:")
-        if not self._conectados:
+        print(f"\nRoteador: {self.nome} | {self.ip_base}.{self.ip}")
+        if not self.conectados:
             print("Nenhum dispositivo conectado.")
             return
 
-        for d in self._conectados:
-            print(f"{d} | Conectado em: {d.data_conexao:%d/%m/%Y %H:%M:%S}")
-
+        for d in self.conectados:
+            print(f"{d.nome} IP | {d.ip} | Conectado em: {d.data_conexao:%d/%m/%Y %H:%M:%S}")
 
 if __name__ == "__main__":
-    r = Roteador("WiFi-Casa", "192.168.0", "senha123")
+    r = Roteador("WiFi-Casa", "192.168.0", 1, "123")
 
     notebook = Dispositivo("Dell Inspiron", "Notebook")
     celular = Dispositivo("iPhone 13", "Smartphone")
 
-    notebook_conectado = r.conectar(notebook, "senha123")
-    celular_conectado = r.conectar(celular, "errado123")
+    r.conectar(notebook, "123")
+    r.conectar(celular, "321")
 
     r.listar_dispositivos()
